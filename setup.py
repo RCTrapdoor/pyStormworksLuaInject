d229d43c1afec6d577419dfd59b7d7995fec577e
Trapdoor <5636136+RCTrapdoor@users.noreply.github.com>
2022-02-12 19:22:27 +0100

    Bumped version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # This call to setup() does all the work
 setup(
     name="pyStormworksLuaInject",
-    version="0.1.3",
+    version="0.1.5",
     packages=find_packages(),
     install_requires=[
         'vdf;platform_system=="Linux"'
